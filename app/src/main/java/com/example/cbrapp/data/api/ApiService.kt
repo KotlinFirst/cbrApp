85d849477f6d55065371392890c9a845521b2e87
KotlinFirst <muravev.2017@bk.ru>
2022-03-25 17:45:42 +0300

    JSON object was processing correctly

--- app/src/main/java/com/example/cbrapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/cbrapp/data/api/ApiService.kt
@@ -1,10 +1,12 @@
 package com.example.cbrapp.data.api
 
+import com.example.cbrapp.data.ExampleJson
 import com.example.cbrapp.data.Valute
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("daily_json.js")
-    fun getValute(): List<Valute>
+    fun getValute(): Single<ExampleJson>
 }
--- app/src/main/java/com/example/cbrapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cbrapp/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.cbrapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.cbrapp.R
 import com.example.cbrapp.data.api.ApiFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ApiFactory.apiService.getValute()
-            .subscribe
+        val disposable = ApiFactory.apiService.getValute()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
 
     }
 
